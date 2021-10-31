f0324bd73f136cdf00c58b7099abf219788ff92c
Rashedul Islam Hridoy <hridoy.developer@yahoo.com>
2021-10-31 13:34:31 +0600

    Question List Updated

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,70 +1,348 @@
 const Data = [
     {
       id: 1,
-      question: "Rolex is a company that specializes in what type of product?",
+      question: "Which type of JavaScript language is ___ ?",
       answers: [
         {
-          text: "Phone",
+          text: "Object-Oriented",
           correct: false,
         },
         {
-          text: "Watches",
+          text: "Object-Based",
           correct: true,
         },
         {
-          text: "Food",
+          text: "Assembly-language",
           correct: false,
         },
         {
-          text: "Cosmetic",
+          text: "High-level",
           correct: false,
         },
       ],
     },
     {
       id: 2,
-      question: "When did the website `Facebook` launch?",
+      question: "Which one of the following also known as Conditional Expression:",
       answers: [
         {
-          text: "2004",
+          text: "Alternative to if-else",
+          correct: false,
+        },
+        {
+          text: "Switch statement",
+          correct: false,
+        },
+        {
+          text: "If-then-else statement",
+          correct: false,
+        },
+        {
+          text: "immediate if",
           correct: true,
-        },
-        {
-          text: "2005",
-          correct: false,
-        },
-        {
-          text: "2006",
-          correct: false,
-        },
-        {
-          text: "2007",
-          correct: false,
         },
       ],
     },
     {
       id: 3,
-      question: "Who played the character of harry potter in movie?",
+      question: "In JavaScript, what is a block of statement?",
       answers: [
         {
-          text: "Johnny Deep",
+          text: "Conditional block",
           correct: false,
         },
         {
-          text: "Leonardo Di Caprio",
-          correct: false,
-        },
-        {
-          text: "Denzel Washington",
-          correct: false,
-        },
-        {
-          text: "Daniel Red Cliff",
+          text: "block that combines a number of statements into a single compound statement",
           correct: true,
         },
+        {
+          text: "both conditional block and a single statement",
+          correct: false,
+        },
+        {
+          text: "block that contains a single statement",
+          correct: true,
+        },
+        
       ],
     },
+    {
+      id: 4,
+      question: "When interpreter encounters an empty statements, what it will do:",
+      answers: [
+        {
+          text: "Shows a warning",
+          correct: false,
+        },
+        {
+          text: "Prompts to complete the statement",
+          correct: false,
+        },
+        {
+          text: "Throws an error",
+          correct: false,
+        },
+        {
+          text: "Ignores the statements",
+          correct: true,
+        },
+        
+      ],
+    },
+    {
+      id: 5,
+      question: "In JavaScript, what is a block of statement?",
+      answers: [
+        {
+          text: "Conditional block",
+          correct: false,
+        },
+        {
+          text: "block that combines a number of statements into a single compound statement",
+          correct: true,
+        },
+        {
+          text: "both conditional block and a single statement",
+          correct: false,
+        },
+        {
+          text: "block that contains a single statement",
+          correct: true,
+        },
+        
+      ],
+    },
+    {
+      id: 6,
+      question: "The 'function' and 'var' are known as:",
+      answers: [
+        {
+          text: "Keywords",
+          correct: false,
+        },
+        {
+          text: "Data types",
+          correct: false,
+        },
+        {
+          text: "Declaration statement",
+          correct: true,
+        },
+        {
+          text: "Prototype",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 7,
+      question: "Which of the following variables takes precedence over the others if the names are the same?",
+      answers: [
+        {
+          text: "Global variable",
+          correct: false,
+        },
+        {
+          text: "The local element",
+          correct: true,
+        },
+        {
+          text: "The two of the above",
+          correct: false,
+        },
+        {
+          text: "None of the above",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 8,
+      question: "Which one of the following is the correct way for calling the JavaScript code?",
+      answers: [
+        {
+          text: "Preprocessor",
+          correct: false,
+        },
+        {
+          text: "Triggering Event",
+          correct: false,
+        },
+        {
+          text: "RMI",
+          correct: false,
+        },
+        {
+          text: "Function/Method",
+          correct: true,
+        },
+        
+      ],
+    },
+    {
+      id: 9,
+      question: "Which of the following type of a variable is volatile?",
+      answers: [
+        {
+          text: "Mutable variable",
+          correct: true,
+        },
+        {
+          text: "Dynamic variable",
+          correct: false,
+        },
+        {
+          text: "Volatile variable",
+          correct: false,
+        },
+        {
+          text: "Immutable variable",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 10,
+      question: "Which of the following option is used as hexadecimal literal beginning?",
+      answers: [
+        {
+          text: "00",
+          correct: false,
+        },
+        {
+          text: "0x",
+          correct: false,
+        },
+        {
+          text: "0X",
+          correct: false,
+        },
+        {
+          text: "Both 0x and 0X",
+          correct: true,
+        },
+        
+      ],
+    },
+    {
+      id: 11,
+      question: "When there is an indefinite or an infinite value during an arithmetic computation in a program, then JavaScript prints______.",
+      answers: [
+        {
+          text: "Prints an exception error",
+          correct: false,
+        },
+        {
+          text: "Prints an overflow error",
+          correct: false,
+        },
+        {
+          text: "Displays 'Infinity'",
+          correct: true,
+        },
+        {
+          text: "Prints the value as such",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 12,
+      question: "n the JavaScript, which one of the following is not considered as an error:",
+      answers: [
+        {
+          text: "Syntax error",
+          correct: false,
+        },
+        {
+          text: "Missing of semicolons",
+          correct: false,
+        },
+        {
+          text: "Division by zero",
+          correct: true,
+        },
+        {
+          text: "Missing of Bracket",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 13,
+      question: "Which of the following givenfunctions of the Number Object formats a number with a different number of digits to the right of the decimal?",
+      answers: [
+        {
+          text: "toExponential()",
+          correct: false,
+        },
+        {
+          text: "toFixed()",
+          correct: true,
+        },
+        {
+          text: "toPrecision()",
+          correct: false,
+        },
+        {
+          text: "toLocaleString()",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 14,
+      question: "Which of the following number object function returns the value of the number?",
+      answers: [
+        {
+          text: "toString()",
+          correct: false,
+        },
+        {
+          text: "valueOf()",
+          correct: true,
+        },
+        {
+          text: "toLocaleString()",
+          correct: false,
+        },
+        {
+          text: "toPrecision()",
+          correct: false,
+        },
+        
+      ],
+    },
+    {
+      id: 15,
+      question: "Which of the following function of the String object returns the character in the string starting at the specified position via the specified number of characters?",
+      answers: [
+        {
+          text: "slice()",
+          correct: false,
+        },
+        {
+          text: "split()",
+          correct: false,
+        },
+        {
+          text: "substr()",
+          correct: true,
+        },
+        {
+          text: "search()",
+          correct: false,
+        },
+        
+      ],
+    }
+    
   ];
 
   export default Data;
